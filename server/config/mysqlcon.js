bfc4f9a6619aefde679d7e58cea22b6205a2a69b
NerdChanOjas <ojas.off2003@gmail.com>
2024-02-28 11:10:17 +0530

    connection error TODO FIX

--- server/config/mysqlcon.js
+++ server/config/mysqlcon.js
@@ -1,10 +1,15 @@
 import Sequelize from "sequelize";
 
-const sequelize = new Sequelize('logipulse', 'root', 'tiger', {
-    host: 'localhost',
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.USERNAME, process.env.PASSWORD, {
+    host: process.env.HOST_NAME,
     dialect:'mysql'
 });
 
+// const sequelize = new Sequelize('logipulse', 'root', 'tiger', {
+//     host: 'localhost',
+//     dialect:'mysql'
+// });
 // const sequelize = new Sequelize(`mysql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`)
 
 sequelize
