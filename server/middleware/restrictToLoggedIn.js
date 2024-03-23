9f8161be8197e583369720435094e3460f825ed8
NerdChanOjas <ojas.off2003@gmail.com>
2024-03-23 14:16:21 +0530

    verify user error check

--- server/middleware/restrictToLoggedIn.js
+++ server/middleware/restrictToLoggedIn.js
@@ -3,11 +3,11 @@ import { verifyUser } from "../utils/jwtHelper.js";
 export const restrictToLoggedInUser = (req, res, next) => {
     // * check if Uid exist
     const userUid = req.cookies?.uid;
-    if (!userUid) return res.send("/login");
+    if (!userUid) return res.send("/first");
 
     // * check if the uid is valid
     const user = verifyUser(userUid);
-    if (!user) return res.send("/login");
+    if (!user) return res.send("/second");
 
     // * forward the payload API
     req.user = user;
