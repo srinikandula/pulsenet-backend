a8f2a5bcda98bf82e1e96a63c6d02e604ee50b90
minakshibhati <25007543@mahindralogistics.com>
2024-04-05 18:24:01 +0530

    SMTP configuration

--- server/config/mailConfig.js
+++ server/config/mailConfig.js
@@ -3,10 +3,10 @@ import nodemailer from "nodemailer";
 let transporter = nodemailer.createTransport({
     host: `${process.env.SMTP_SERVER}`,
     port: `${process.env.SMTP_PORT}`,
-    secure:false,
+    secure:true,
     logger:true,
     debug:true,
-    secureConnection: false,
+    secureConnection: true,
     auth: {
         user: `${process.env.EMAIL_ADDRESS}`,
         pass: `${process.env.EMAIL_PASSWORD}`
