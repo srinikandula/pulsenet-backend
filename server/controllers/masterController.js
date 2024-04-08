8d08981b5cede15546685898e1c6ea69aa5e05e3
minakshibhati <25007543@mahindralogistics.com>
2024-04-08 16:51:35 +0530

    Location API

--- server/controllers/masterController.js
+++ server/controllers/masterController.js
@@ -1,18 +1,21 @@
 import expressAsyncHandler from "express-async-handler";
 import db from "../config/mysqlcon.js";
 import errorCodes from "../constants.js";
-import { errorFormat, successFormat } from '../middleware/formatResponse.js'
+import {
+    errorFormat,
+    successFormat
+} from '../middleware/formatResponse.js'
 
 export const saveVertical = expressAsyncHandler(async (req, res) => {
     try {
         let resp = await db.sequelize.query(`CALL SP_Vertical(:_User_ID, :_Is_Admin, :_Mode, :_Vertical_Id,:_Vertical_Name,:_Vertical_Status)`, {
             replacements: {
-                _User_ID: req.body.User_ID? req.body.User_ID : 0,
-                _Is_Admin: req.body.Is_Admin? req.body.Is_Admin : 1,
-                _Mode: req.body.Mode? req.body.Mode : 'INSERTUPDATE',
-                _Vertical_Id: req.body.Vertical_Id? req.body.Vertical_Id : 0,
-                _Vertical_Name: req.body.Vertical_Name? req.body.Vertical_Name : '',
-                _Vertical_Status: req.body.Vertical_Status? req.body.Vertical_Status : '',
+                _User_ID: req.body.User_ID ? req.body.User_ID : 0,
+                _Is_Admin: req.body.Is_Admin ? req.body.Is_Admin : 1,
+                _Mode: req.body.Mode ? req.body.Mode : 'INSERTUPDATE',
+                _Vertical_Id: req.body.Vertical_Id ? req.body.Vertical_Id : 0,
+                _Vertical_Name: req.body.Vertical_Name ? req.body.Vertical_Name : '',
+                _Vertical_Status: req.body.Vertical_Status ? req.body.Vertical_Status : '',
             },
             type: db.sequelize.QueryTypes.INSERT
         })
@@ -73,7 +76,7 @@ export const getAddUpdateService = expressAsyncHandler(async (req, res) => {
 
 });
 
-export const getAddUpdateSubCluster = expressAsyncHandler(async(req, res) => {
+export const getAddUpdateSubCluster = expressAsyncHandler(async (req, res) => {
     try {
         const ClusterData = await db.sequelize.query(`Call SP_SubCluster(:_User_ID,:_Is_Admin,:_Mode,:_SubCluster_Id,:_SubCluster_Name,:_Cluster_Id,:_SubCluster_Status)`, {
             replacements: {
@@ -87,6 +90,29 @@ export const getAddUpdateSubCluster = expressAsyncHandler(async(req, res) => {
                 _Cluster_Id: req.body.Cluster_Id ? req.body.Cluster_Id : 0,
                 _Cluster_Name: req.body.Cluster_Name ? req.body.Cluster_Name : '',
                 _SubCluster_Status: req.body.SubCluster_Status ? req.body.SubCluster_Status : ''
+            }
+        });
+        res.status(200).send(successFormat("Success", "Success", ClusterData, []));
+    } catch (error) {
+        res.status(401).send(errorFormat("Error", error.message, {}, [], 401));
+    }
+});
+
+export const getAddUpdateLocation = expressAsyncHandler(async (req, res) => {
+    try {
+        const ClusterData = await db.sequelize.query(`Call SP_Location(:_User_ID,:_Is_Admin,:_Mode,:_Location_Id,:_Cluster_Id,:_SubCluster_Id,:_Vertical_Id,:_Location_Name,:_Location_Status)`, {
+            replacements: {
+                _User_ID: req.body.User_ID ? req.body.User_ID : '',
+                _Is_Admin: req.body.Is_Admin ? req.body.Is_Admin : '',
+                _Mode: req.body.Mode ? req.body.Mode : '',
+                //Mode- GET (Get all Location)
+                //Mode - INSERTUPDATE (add or update the Location )
+                _Location_Id: req.body._Location_Id ? req.body._Location_Id : 0,
+                _Cluster_Id: req.body.Cluster_Id ? req.body.Cluster_Id : 0,
+                _SubCluster_Id: req.body.SubCluster_Id ? req.body.SubCluster_Id : 0,
+                _Vertical_Id: req.body.Vertical_Id ? req.body.Vertical_Id : 0,
+                _Location_Name: req.body.Location_Name ? req.body.Location_Name : '',
+                _Location_Status: req.body.Location_Status ? req.body.Location_Status : ''
             }
         });
         res.status(200).send(successFormat("Success", "Success", ClusterData, []));
